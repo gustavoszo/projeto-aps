f67802f5876fd5e4ec72973e329336d81b596f1f
Gustavo <g_souza14@outlook.com>
2024-03-15 00:23:06 -0300

    project initialzr

---
+++ src/main/java/com/projetoaps/demoprojetoaps/DemoProjetoapsApplication.java
@@ -0,0 +1,13 @@
+package com.projetoaps.demoprojetoaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjetoapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjetoapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoaps/demoprojetoaps/DemoProjetoapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoaps.demoprojetoaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjetoapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
